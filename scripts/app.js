e9f5d21fdf5a9807162b678b75826d36f792db28
Alessandro Fulgini <fuljo97@gmail.com>
2021-07-02 16:42:41 +0200

    Force removal of non-assigned texture maps from materials

--- scripts/app.js
+++ scripts/app.js
@@ -46,16 +46,27 @@ async function configureScene(scene) {
         // Get the model by name
         let name = modelConfig.name;
         let model = models[name];
-
-        // Type of this model (tree, bird, ...)
-        model.type = modelConfig.type
-        // TODO: Add data according to type (e.g. attach points for trees ...)
-
+        
         // Insert into the dictionary of scene models
         if (!scene.models.has(name)) {
             scene.models.set(name, model);
         } else {
             console.error(`Model \"${name}}\" already exists`);
+        }
+
+        // Add type of this model (tree, bird, ...)
+        model.type = modelConfig.type
+        // TODO: Add data according to type (e.g. attach points for trees ...)
+
+        // Remove non-assigned maps from the materials
+        for (let mtl of Object.values(model.materialsByIndex)) {
+            // Iterate over the properties of each material
+            for (let key in mtl) {
+                // If a map exists, but has no texture assigned, then it is useless
+                if (key.startsWith("map") && !mtl[key].texture) {
+                    delete mtl[key];
+                }
+            }
         }
 
         // Select the rendering program
