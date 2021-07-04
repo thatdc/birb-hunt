ddf881e726774bcc27fce78e435e8d1b6a27f8a6
Alessandro Fulgini <fuljo97@gmail.com>
2021-07-04 23:01:33 +0200

    Add constructor for Node and SceneObject
    - Somse attributes in Node and SceneObject are now part of Mesh,
    so they have been removed

--- scripts/scene.js
+++ scripts/scene.js
@@ -1,33 +1,99 @@
 class Node {
-    // List of child nodes (will be transormed)
-    children = [];
-
-    // Transform of the object w.r.t. the parent
-    localMatrix = null;
-
-    // Transform of the object w.r.t. the world origin
-    worldMatrix = null;
+    /**
+     * Unique of the object in the scene
+     * @type {string}
+     */
+    name;
 
     /**
-     * Scale factor of this model
-     * @type {int}
+     * List of child nodes (will be transormed)
+     * @type {Node[]}
+     */
+    children;
+
+    /**
+     * Transform of the object w.r.t. the parent
+     * @type {number[]}
+     */
+    localMatrix;
+
+    /**
+     * Transform of the object w.r.t. the world origin
+     * @type {number[]}
+     */
+    worldMatrix;
+
+    /**
+     * Scale factor of this model [X Y Z]
+     * @type {number[]}
      */
     scale;
 
     /**
      * Position in XYZ coordinates of the object
+     * @type {number[]}
      */
-    position = [];
+    position;
 
     /**
-     * Rotation of the object in XYZ Euler coordinates
+     * Rotation of the object in XYZ Euler coordinates (in degrees)
+     * @type {number[]}
      */
-    rotation = [];
+    rotation;
 
     /**
      * Pointer to the parent
+     * @type {Node}
      */
     parent = null;
+
+    /**
+     * Creates an empty node with no parent and no children.
+     * 
+     * All transformations refer to the parent node.
+     * 
+     * @param {string} name Unique name of this node in the scene
+     * @param {number[]} position XYZ coordinates
+     * @param {number[]} rotation XYZ euler angles, in degrees
+     * @param {number[]} scale XYZ scaling factors
+     */
+    constructor(
+        name,
+        position = [0, 0, 0],
+        rotation = [0, 0, 0],
+        scale = [1, 1, 1]
+    ) {
+        this.name = name;
+        this.position = position;
+        this.rotation = rotation;
+        this.scale = scale;
+        this.parent = null;
+        this.children = [];
+        this.localMatrix = _makeLocal();
+        this.worldMatrix = this.localMatrix;
+    }
+
+    /**
+     * Creates a local transformation matrix from this node's
+     * position, rotation and scale.
+     * @returns {number[]}
+     */
+    _makeLocal() {
+        // Translate
+        var m = utils.MakeTranslateMatrix(...position);
+        // Rotate
+        m = utils.multiplyMatrices(m,
+            utils.MakeRotateZMatrix(rotation[2]));
+        m = utils.multiplyMatrices(m,
+            utils.MakeRotateYMatrix(rotation[1]));
+        m = utils.multiplyMatrices(m,
+            utils.MakeRotateXMatrix(rotation[0]));
+        // Scale
+        m = utils.multiplyMatrices(m,
+            utils.MakeScaleNuMatrix(...scale));
+
+        return m;
+    }
 
     /**
      * Set the parent
@@ -71,34 +137,36 @@ class Node {
 }
 
 class SceneObject extends Node {
-    /** Vertex Array Object
-     * @type {WebGLVertexArrayObject}
+    /** Model associated to this object.
+     * @type {Mesh}
      */
-    vao;
+    model;
 
     /**
-     * WebGL program (Vertex shader + fragment shader).
-     * @type {Program}
+     * Creates an object with no parent and no children.
+     * 
+     * It is assumed that the given model has been already initialized with
+     * the VAO, the element array buffers and textures.
+     * 
+     * All transformations refer to the parent node.
+     * 
+     * @param {string} name Unique name of this node in the scene
+     * @param {Mesh} model Model associated to this object
+     * @param {number[]} position XYZ coordinates
+     * @param {number[]} rotation XYZ euler angles, in degrees
+     * @param {number[]} scale XYZ scaling factors
+     * @param {number[]} localMatrix Transform of the object w.r.t. the parent
      */
-    program;
-
-    /** WebGL diffuse texture
-     * @type {WebGLTexture}
-     */
-    diffuseMap;
-
-    /**
-     * WebGL normal map.
-     * If not set, the vertex normals will be used.
-     * @type {WebGLTexture}
-     */
-    normalMap;
-
-    /**
-     * Length of the Elements buffer
-     * @type {int}
-     */
-    bufferLength;
+    constructor(
+        name,
+        model,
+        position = [0, 0, 0],
+        rotation = [0, 0, 0],
+        scale = [1, 1, 1]
+    ) {
+        super(name, position, rotation, scale);
+        this.model = model;
+    }
 
     /**
      * Create a new scene object from a JSON description
@@ -115,13 +183,6 @@ class Scene {
      * @type {Map<String, Mesh>}
      */
     models = new Map();
-
-    /**
-     * Dictionary containing all the Vertex Array Objects of this scene.
-     * @type {Map<String, WebGLVertexArrayObject>}
-     */
-    /* TODO: Do we still need this? */
-    vaos = new Map();
 
     /**
      * Dictionary containing the program objects.
