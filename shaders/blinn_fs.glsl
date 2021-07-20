9963a66aca940fa4fcb0c1d845975d2c0e24dd18
Alessandro Fulgini <fuljo97@gmail.com>
2021-07-20 19:41:03 +0200

    Add missing shader changes

--- shaders/blinn_fs.glsl
+++ shaders/blinn_fs.glsl
@@ -33,7 +33,7 @@ uniform vec3 u_specular;
 uniform vec4 u_highlightColor;
 
 // Specular info
-// uniform float u_refractionIndex;
+uniform float u_sharpness;
 uniform float u_specularExponent;
 
 // Maps
@@ -116,15 +116,10 @@ void main() {
   vec3 mtl_diffuse = b_useMapDiffuse ? texture(u_mapDiffuse, fs_uv).rgb : u_diffuse;
 
   // Get the specular exponent (from map or material)
-  float specular_exponent = b_useMapSpecularExponent ? texture(u_mapSpecularExponent, fs_uv).r : u_specularExponent;
-
-  // Approximate the roughness in [0, 1] range
-  // by inverting the empirical formula used by the blender OBJ exporter
-  // https://github.com/blender/blender-addons/blob/692a15f3c50c57cc3b66115aa5f316aa290ddc0a/io_scene_obj/export_obj.py#L77
-  float roughness = clamp(1. - (specular_exponent / 30.), 0. , 1.);
+  float specular_exponent = u_specularExponent * (b_useMapSpecularExponent ? texture(u_mapSpecularExponent, fs_uv).r : 1.);
 
   // Ambient light (select mipmap level according to roughness)
-  float lod = roughness * 7.;
+  float lod = (1. - u_sharpness) * 7.;
   vec3 ambient = mtl_diffuse * u_ambientLight * u_ambient * textureLod(u_mapEnv, n_normal, lod).rgb;
 
   // Diffuse and specular accumulator
